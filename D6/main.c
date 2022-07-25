7bbb64e680d8499cd26db0a5781bdc00c69b5af2
paul <prm85@nau.edu>
2022-07-25 15:18:25 -0700

    added some comments to help explain what I was trying to fufill with this code

--- D6/main.c
+++ D6/main.c
@@ -14,41 +14,40 @@ typedef struct gasStationStruct
     double price;
     struct gasStationStruct *nextPtr;
    } gasStationType;
-
+// these functions are Paul's code
 gasStationType *storeList(gasStationType *headPtr, FILE *file);
 gasStationType *initializeIterNode();
+
+
 int main( int argc, char *argv[] )
    {
-
-        gasStationType *headPtr = (gasStationType *)malloc(sizeof(gasStationType));
         FILE *file = fopen(filename, "r");
+        gasStationType *headPtr = (gasStationType *)malloc(sizeof(gasStationType));
 
-        //openInputFile(fileName);
-        // first we need to store the data into the 2D char array
         printf("list of gas stations near NAU\n\n");
-
+        // first we need to store the data into the linked list
         storeList(headPtr, file);
 
         return 0;
-
    }
 
    gasStationType *storeList(gasStationType *headPtr, FILE *file)
    {
 
-     //char line;
+     // initialize wkgPtr
      gasStationType *wkgPtr = initializeIterNode();
      fgets(headPtr->stationName, 100, file);
      // debug printf
      printf("%s", headPtr->stationName);
      wkgPtr = headPtr;
-     // debug
-     //printf("%s", wkgPtr->stationName);
-     readStringToLineEndFromFile(wkgPtr->stationName);
+     // debug printf
+     // printf("%s", wkgPtr->stationName);
+     // read each line in file until NULL
      while(fgets(wkgPtr->stationName, 100, file) != NULL)
      {
        wkgPtr = wkgPtr->nextPtr;
        readStringToLineEndFromFile(wkgPtr->stationName);
+       // debug printf
        printf("%s", wkgPtr->stationName);
      }
 
